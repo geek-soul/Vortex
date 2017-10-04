06a05a86e255e0717c8517dfd8a1fa084f22726e
wangmingyong <my.want@icloud.com>
2017-10-05 00:36:04 +0800

    1.add startCamera btn in welcom activity,change camera view to another activity. 2.solve the problem of land-screen and port-screen

--- app/src/main/java/vortex/geek/com/vortex/MainActivity.java
+++ app/src/main/java/vortex/geek/com/vortex/MainActivity.java
@@ -1,48 +1,33 @@
 package vortex.geek.com.vortex;
 
-import android.Manifest;
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.content.pm.PackageManager;
-import android.graphics.BitmapFactory;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.FrameLayout;
-import android.widget.ImageView;
-import android.widget.Toast;
 
 import com.tencent.bugly.crashreport.CrashReport;
 
-import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
-import vortex.geek.com.vortex.core.CameraTextureView;
-
-public class MainActivity extends Activity implements View.OnClickListener {
-
+public class MainActivity extends Activity {
     private int[] mImgIds = new int[] { R.mipmap.welcome,
             R.mipmap.gohomepage};
     private List<View> mLaunchViews = new ArrayList<View>();
     private ViewPager mViewPager;
-
+    private View view1,view2;
     private FrameLayout rgbPanle;
 
 
-    private ImageView mCaptureImage;
-    private CameraTextureView mCameraTextureView;
 
-    private static final int REQUEST_CAMERA_PERMISSION = 1;
-
+    private Button startBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -89,17 +74,27 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
         NativeFFMPEGMethod instance = new NativeFFMPEGMethod();
 
+        InitView();
+        setListener();
 
-        File mFile = new File(getExternalFilesDir(null), "pic.jpg");
-        findViewById(R.id.picture).setOnClickListener(this);
-        findViewById(R.id.info).setOnClickListener(this);
 
-        mCaptureImage = (ImageView) findViewById(R.id.iv_capture_pic);
-        mCameraTextureView = (CameraTextureView) findViewById(R.id.texture);
 
-        //配置CameraTexture
-        mCameraTextureView.setActivity(this);
-        mCameraTextureView.setPicSaveFile(mFile);
+
+    }
+
+    private void setListener() {
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, CameraActivity.class);
+                startActivity(intent);
+               // MainActivity.this.finish();
+            }
+        });
+    }
+
+    private void InitView() {
+        startBtn = (Button)view1.findViewById(R.id.show);
 
     }
 
@@ -110,7 +105,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
             //ImageView imageView = new ImageView(getApplicationContext());
             //imageView.setScaleType(ScaleType.CENTER_CROP);
             //imageView.setImageResource(imgId);
-            View view1,view2;
+
             LayoutInflater inflater=getLayoutInflater();
             view1 = inflater.inflate(R.layout.acttivty_welcome, null);
             view2 = inflater.inflate(R.layout.activty_gohomepage, null);
@@ -118,108 +113,6 @@ public class MainActivity extends Activity implements View.OnClickListener {
             mLaunchViews.add(view2);
 
         }
-    }
-
-
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.picture: {
-                mCameraTextureView.takePicture(new CameraTextureView.TackPhotoCallback() {
-                    @Override
-                    public void tackPhotoSuccess(String photoPath) {
-                        showToast(photoPath);
-                        mCaptureImage.setImageBitmap(BitmapFactory.decodeFile(photoPath));
-                    }
-
-                    @Override
-                    public void tackPhotoError(Exception e) {
-                        showToast(e.getMessage());
-                    }
-                });
-                break;
-            }
-            case R.id.info: {
-                new AlertDialog.Builder(this)
-                        .setMessage(R.string.intro_message)
-                        .setPositiveButton(android.R.string.ok, null)
-                        .show();
-                break;
-            }
-        }
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            requestCameraPermission();
-            return;
-        }
-        mCameraTextureView.onResume();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        mCameraTextureView.onPause();
-    }
-
-
-    private void requestCameraPermission() {
-        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
-            ConfirmationDialog();
-        } else {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA},
-                    REQUEST_CAMERA_PERMISSION);
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
-                                           @NonNull int[] grantResults) {
-        if (requestCode == REQUEST_CAMERA_PERMISSION) {
-            if (grantResults.length != 1 || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
-                Toast.makeText(getApplicationContext(), R.string.request_permission, Toast.LENGTH_SHORT).show();
-            } else {
-                //执行相机初始化操作
-                mCameraTextureView.onResume();
-            }
-        } else {
-            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        }
-    }
-
-    private void ConfirmationDialog() {
-        AlertDialog alertDialog = new AlertDialog.Builder(this)
-                .setMessage(R.string.request_permission)
-                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        ActivityCompat.requestPermissions(MainActivity.this,
-                                new String[]{Manifest.permission.CAMERA},
-                                REQUEST_CAMERA_PERMISSION);
-                    }
-                })
-                .setNegativeButton(android.R.string.cancel,
-                        new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                Toast.makeText(getApplicationContext(), R.string.request_permission, Toast.LENGTH_SHORT).show();
-                            }
-                        })
-                .create();
-        alertDialog.show();
-    }
-
-
-    /**
-     * Shows a {@link Toast} on the UI thread.
-     *
-     * @param text The message to show
-     */
-    private void showToast(final String text) {
-        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
     }
 
 
